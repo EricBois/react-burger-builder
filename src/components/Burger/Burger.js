547e319626d7a75d4605754e4edd5431eb96d109
Eric Boisvenu <burn4live@gmail.com>
2020-04-22 16:15:23 -0600

    Added control module

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import classes from './Burger.module.css';
 import Ingredients from './Ingredients/Ingredients';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <Ingredients key={igKey + i} type={igKey} />;
       });
-    });
+    }).reduce((arr,el) => {
+      return arr.concat(el)
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients.</p>
+  }
   return (
     <div className={classes.Burger}>
       <Ingredients type="bread-top" />
