d7a2be8e78bc928834ac6c5b363c5c9e8df3ff21
Eric Boisvenu <burn4live@gmail.com>
2020-04-23 15:57:03 -0600

    added state functions, modal and summary

--- src/components/Burger/BuildControls.js
+++ src/components/Burger/BuildControls.js
@@ -11,9 +11,17 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price}</strong> </p>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl 
+        key={ctrl.label}
+        label={ctrl.label}
+        deleted={() => props.remove(ctrl.type)}
+        added={() => props.add(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
+    <button disabled={!props.purchasable} className={classes.OrderButton}>ORDER NOW</button>
   </div>
 );
 
