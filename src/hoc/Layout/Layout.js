db0a1f495d60ad274c2a68868746e17b6a25c243
Eric Boisvenu <burn4live@gmail.com>
2020-04-26 16:02:06 -0600

    making it more responsive, switching folders, added sideDrawer, reusable items

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Aux from '../Aux/Aux';
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+  
+  state = {
+    showSideDrawer: false
+  }
+
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  SideDrawertoggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer};
+    });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.SideDrawertoggleHandler}/>
+        <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler}/>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
